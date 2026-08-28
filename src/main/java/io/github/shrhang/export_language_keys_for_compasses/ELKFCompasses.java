be87e1f3c0905000c9323a355860fb5b02bbf4d4
ShrHang <shhshrhang@gmail.com>
2026-08-28 22:03:08 +0800

    init

---
+++ src/main/java/io/github/shrhang/export_language_keys_for_compasses/ELKFCompasses.java
@@ -0,0 +1,11 @@
+package io.github.shrhang.export_language_keys_for_compasses;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ELKFCompasses.MODID)
+public class ELKFCompasses {
+
+    public static final String MODID = "export_language_keys_for_compasses";
+
+    public ELKFCompasses() {}
+}
